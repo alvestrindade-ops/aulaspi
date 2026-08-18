68078baef7bc5b612650d758b98b206ad68ba368
Bruno Felipe <alves.trindade@escolar.ifrn.edu.br>
2026-08-18 10:08:15 -0300

    Colocando os arquivos inicias da minha aplicação

---
+++ src/main/java/ifrn/pi/Eventos/controllers/indexController.java
@@ -0,0 +1,14 @@
+package ifrn.pi.Eventos.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+
+	@RequestMapping("/")
+	public String index() {
+		System.out.println("Chamou o método index");
+		return "home";
+	}
+}
