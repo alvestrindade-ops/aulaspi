ea133ae319dc8d946ee8ec424c429454e02486b4
Bruno Felipe <alves.trindade@escolar.ifrn.edu.br>
2026-08-24 20:42:05 -0300

    Criando o controller de eventos

---
+++ src/main/java/ifrn/pi/Eventos/controllers/EventoController.java
@@ -0,0 +1,13 @@
+package ifrn.pi.Eventos.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class EventoController {
+
+	@RequestMapping
+	public String form() {
+		return "formEvento";
+	}
+}
