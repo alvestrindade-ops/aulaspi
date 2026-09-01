e0a337eeda974bad17398e02534cb3fb1e833817
Bruno Felipe <alves.trindade@escolar.ifrn.edu.br>
2026-08-31 23:58:10 -0300

    Enviando dados do formulário para a aplicação

--- src/main/java/ifrn/pi/Eventos/controllers/EventoController.java
+++ src/main/java/ifrn/pi/Eventos/controllers/EventoController.java
@@ -3,11 +3,24 @@ package ifrn.pi.Eventos.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ifrn.pi.Eventos.models.Evento;
+
 @Controller
 public class EventoController {
 
 	@RequestMapping("/eventos/form")
 	public String form() {
 		return "formEvento";
+	}
+
+	@RequestMapping("/eventos")
+	public String salvar(Evento evento) {
+
+		System.out.println("Nome: " + evento.getNome());
+		System.out.println("Local: " + evento.getLocal());
+		System.out.println("Data: " + evento.getData());
+		System.out.println("Horário: " + evento.getHorario());
+
+		return "eventoSalvo";
 	}
 }
